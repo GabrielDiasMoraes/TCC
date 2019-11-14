5f3f93f177682a6f46f5709fb6efc467ad89f963
Gabriel Dias Moraes <gaabriel.mooraes@gmail.com>
2019-11-14 19:34:55 -0300

    Apresentação

---
+++ Assets/Screenshot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour
+{
+
+    [SerializeField] private string printName;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse2)) 
+        {
+            Debug.Log(Application.persistentDataPath);
+            ScreenCapture.CaptureScreenshot(printName +".png");
+        }
+        
+    }
+}
